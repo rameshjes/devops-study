bfbcc3146f4e2cf184ebfe088b7f6d61b07ed11c
Ramesh <rameshkjes@gmail.com>
2020-12-28 00:17:58 +0100

    add test case for subtract

--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -3,6 +3,7 @@ import numpy as np
 from numpy.testing import assert_array_equal
 
 from utilities.utils import add
+from utilities.utils import subtract
 from utilities.utils import remove_spaces
 from utilities.utils import return_string
 from utilities.utils import remove_punctuations
@@ -14,6 +15,10 @@ from utilities.utils import create_array
 def test_add(x, y, result):
     assert add(x, y) == result
 
+@pytest.mark.parametrize('x, y, result', [(18, 10, 8), (12, 88, -76),
+                                          (1.5, 1.0, 0.5)])
+def test_subtract(x, y, result):
+    assert  subtract(x, y) == result
 
 @pytest.mark.parametrize('data, result', [('he ', 'he'), ('h   ello', 'hello'),
                                           (' ', '')])
--- src/utilities/utils.py
+++ src/utilities/utils.py
@@ -7,6 +7,11 @@ def add(x, y):
     return x + y
 
 
+def subtract(x,y):
+
+    return x - y
+
+
 def return_string(data):
 
     return data
