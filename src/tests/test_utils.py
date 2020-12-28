2fd1f0f93f948db8962100eeb6ab2adb4f84e09b
Ramesh Kumar <rameshjes@users.noreply.github.com>
2020-12-28 00:25:03 +0000

    Automated formating of code using PEP8 standards

--- src/tests/test_utils.py
+++ src/tests/test_utils.py
@@ -9,33 +9,32 @@ from utilities.utils import return_string
 from utilities.utils import subtract
 
 
-@pytest.mark.parametrize("x, y, result", [(18, 10, 28), (12, 88, 100), (0.5, 1.4, 1.9)])
+@pytest.mark.parametrize("x, y, result", [(18, 10, 28), (12, 88, 100),
+                                          (0.5, 1.4, 1.9)])
 def test_add(x, y, result):
     assert add(x, y) == result
 
 
-@pytest.mark.parametrize("x, y, result", [(18, 10, 8), (12, 88, -76), (1.5, 1.0, 0.5)])
+@pytest.mark.parametrize("x, y, result", [(18, 10, 8), (12, 88, -76),
+                                          (1.5, 1.0, 0.5)])
 def test_subtract(x, y, result):
     assert subtract(x, y) == result
 
 
-@pytest.mark.parametrize(
-    "data, result", [("he ", "he"), ("h   ello", "hello"), (" ", "")]
-)
+@pytest.mark.parametrize("data, result", [("he ", "he"), ("h   ello", "hello"),
+                                          (" ", "")])
 def test_remove_spaces(data, result):
     assert (remove_spaces(data)) == result
 
 
-@pytest.mark.parametrize(
-    "data, result", [("hello", "hello"), ("lok", "lok"), ("ch ck", "ch ck")]
-)
+@pytest.mark.parametrize("data, result", [("hello", "hello"), ("lok", "lok"),
+                                          ("ch ck", "ch ck")])
 def test_return_string(data, result):
     assert (return_string(data)) == result
 
 
-@pytest.mark.parametrize(
-    "text, output", [("he? wasup", "he wasup"), ("lost...", "lost")]
-)
+@pytest.mark.parametrize("text, output", [("he? wasup", "he wasup"),
+                                          ("lost...", "lost")])
 def test_remove_punctuations(text, output):
     assert (remove_punctuations(text)) == output
 
